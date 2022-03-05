5b26508c162c7a81011f9deac2fd1cb877fe42aa
晓凹凸曼 <szy110110@qq.com>
2022-03-05 20:03:33 +0800

    脚本修复

--- docs/change.sh
+++ docs/change.sh
@@ -1,18 +1,11 @@
-###
- # @Author: szx
- # @Date: 2021-08-10 23:40:11
- # @LastEditTime: 2021-09-21 16:31:46
- # @Description: 替换wp-includes/class-wp-xmlrpc-server.php
- # 需要把文件放到wordpress下执行
- # @FilePath: \push-markdown\docs\change.sh
-### 
+# 替换wp-includes/class-wp-xmlrpc-server.php
+# 需要把文件放到wordpress下执行
 path=wp-includes/class-wp-xmlrpc-server.php
 text1='$bits = $data'
 text2='#$bits = $data'
 text3='\t\t$bits = base64_decode($data['\'bits\'']);'
 text4='$upload_err = apply_filters'
 text5='\t\tif ( !empty($data['\'overwrite\'']) && ($data['\'overwrite\''] == true) ) {\n\t\t\t$old_file = $wpdb->get_row("SELECT ID FROM {$wpdb->posts} WHERE post_title = '\'{\$name}\'' AND post_type = '\'attachment\'' "); \n\t\t\twp_delete_attachment($old_file->ID);\n\t\t}'
-
 sed -i '/'"$text1"'/a\'"$text3"'' $path
 sed -i 's/'"$text1"'/'"$text2"'/' $path
 sed -i '/'"$text4"'/i\'"$text5"'' $path
